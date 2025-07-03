ef669e08435a9b4970f68164b290cbff5a41531c
syridit <syridit.prof@gmail.com>
2025-07-03 18:19:24 +0600

    [add] logging and log visualization tool integration

---
+++ src/main/java/com/wsd/ecom/util/filter/RequestIdFilter.java
@@ -0,0 +1,37 @@
+package com.wsd.ecom.util.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author Md. Sadman Yasar Ridit
+ * @email syridit.prof@gmail.com
+ * @since 03 July, 2025
+ */
+
+@Component
+public class RequestIdFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String requestId = UUID.randomUUID().toString();
+        MDC.put("requestId", requestId);
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
