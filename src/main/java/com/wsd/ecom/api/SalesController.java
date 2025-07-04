745cab776a622571012ee96bbeba1a274b857710
syridit <syridit.prof@gmail.com>
2025-07-04 18:35:26 +0600

    [add] controller api test and implementation

--- src/main/java/com/wsd/ecom/api/SalesController.java
+++ src/main/java/com/wsd/ecom/api/SalesController.java
@@ -1,11 +1,16 @@
 package com.wsd.ecom.api;
 
+import com.wsd.ecom.dto.MaxSaleDayDto;
 import com.wsd.ecom.dto.SalesTodayDto;
 import com.wsd.ecom.service.SalesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * @author Md. Sadman Yasar Ridit
@@ -28,6 +33,14 @@ public class SalesController {
     @GetMapping(value = "/today/total")
     public SalesTodayDto getCustomerWishlist() {
         return salesService.getTotalSalesAmountOfToday();
+    }
+
+
+    @GetMapping("/max-sale-day")
+    public MaxSaleDayDto getMaxSaleDay(
+            @RequestParam("from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+            @RequestParam("to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to) {
+        return salesService.getMaxSalesDay(from, to);
     }
 
 }
